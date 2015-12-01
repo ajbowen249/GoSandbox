6965810b1a49e1fc2f2472ed1f1b69864cafca85
ajbowen249 <ajbowen249@gmail.com>
2015-11-30 22:46:41 -0500

    more console work

--- console/console_win.go
+++ console/console_win.go
@@ -4,6 +4,7 @@ package console
 
 // #include <stdio.h>
 // #include <windows.h>
+// #include <conio.h>
 //
 // void MoveTo(SHORT row, SHORT column)
 // {
@@ -12,6 +13,17 @@ package console
 //     Cord.Y = column;
 //     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cord);
 // }
+//
+// char GetKey()
+// {
+//     char character = 0x00;
+//     if(kbhit())
+//     {
+//        character = getch();
+//     }
+//
+//     return character;
+// }
 import "C"
 import "fmt"
 
@@ -19,12 +31,12 @@ type Console struct{
 	NumCols, NumRows int
 }
 
+// Default returns a Console struct where
+// NumCols == 80 and NumRows == 25
 func Default() *Console{
-	con := new(Console)
-	con.NumCols = 80
-	con.NumRows = 25
+	con := Console{80, 25}
 	
-	return con
+	return &con
 }
 
 // MoveTo sets the console cursor postition
@@ -32,9 +44,10 @@ func (con *Console) MoveTo(row int, column int){
 	C.MoveTo(C.short(row), C.short(column))
 }
 
+// ClearScreen blanks out the console window 
+// and returns the cursor to {0, 0}
 func (con *Console) ClearScreen(){
 	numChars := con.NumCols * con.NumRows
-	
 	con.MoveTo(0, 0)
 	
 	for i :=0; i < numChars; i++{
@@ -42,4 +55,21 @@ func (con *Console) ClearScreen(){
 	}
 	
 	con.MoveTo(0, 0)
+}
+
+// GetKey returns a bool indicating whether a key
+// was pressed on the keyboard and a string for
+// which character was pressed. It does not block
+// for input.
+func (con *Console) GetKey() (bool, string){
+	character := ""
+	isHit := false
+	fromC := C.GetKey()
+	
+	if byte(fromC) != 0x00{
+		character = C.GoStringN(&fromC, 1)
+		isHit = true
+	}
+	
+	return isHit, character
 }
