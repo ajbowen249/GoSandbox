d0b3b2f151a76c79c2c03f762bda49476754e30d
Alex Bowen <ajbowen249@gmail.com>
2016-08-15 16:58:06 -0400

    added a way to quit the Rogue test app

--- rogueConsoleTests/rogueConsoleTests.go
+++ rogueConsoleTests/rogueConsoleTests.go
@@ -1,112 +1,115 @@
 package main
 
-import(
-"strings"
-rc "github.com/ajbowen249/GoSandbox/rogueConsole"
-"github.com/ajbowen249/GoSandbox/console"
+import (
+	"strings"
+
+	"github.com/ajbowen249/GoSandbox/console"
+	rc "github.com/ajbowen249/GoSandbox/rogueConsole"
 )
 
-func main(){
+func main() {
 	rCon := setup()
 	console.SetCursorProperties(1, false)
 	console.ClearScreen(80, 25)
-	
+
 	sprite := new(rc.Sprite)
 	sprite.Width = 2
 	sprite.Height = 2
 	sprite.X = 4
 	sprite.Y = 3
-	sprite.SetString( 
+	sprite.SetString(
 		"SS" +
-		"S ")
-		
+			"S ")
+
 	rCon.RegisterSprite(sprite, 0)
-	
+
 	rCon.Draw()
-	
-	for{
+
+	for {
 		isHit, char := console.GetKey()
-		if isHit{
-			
+		if isHit {
+
 			char = strings.ToLower(char)
-			switch char{
-				case "w":
-					if sprite.Y > 0{
-						sprite.Y--
-					}
-				case "a":
-					if sprite.X > 0{
-						sprite.X--
-					}
-				case "s":
-					if sprite.Y < rCon.EnvHeight{
-						sprite.Y++
-					}
-				case "d":
-					if sprite.X < rCon.EnvWidth{
-						sprite.X++
-					}
+			switch char {
+			case "w":
+				if sprite.Y > 0 {
+					sprite.Y--
+				}
+			case "a":
+				if sprite.X > 0 {
+					sprite.X--
+				}
+			case "s":
+				if sprite.Y < rCon.EnvHeight {
+					sprite.Y++
+				}
+			case "d":
+				if sprite.X < rCon.EnvWidth {
+					sprite.X++
+				}
+			case "q":
+				return
 			}
-			
+
 			rCon.Draw()
 		}
 	}
 }
 
-func setup() *rc.RogueConsole{
+func setup() *rc.RogueConsole {
 	con := rc.NewRogueConsole(25, 9, 25, 9)
 
 	bg1 :=
-		"┌───────────────────────┐" + 
-		"│                       │" +
-		"│                       │" +
-		"│                       │" +
-		"│                       │" +
-		"│                       │" +
-		"│                       │" +
-		"│                       │" +
-		"└───────────────────────┘"
-		
+		"┌───────────────────────┐" +
+			"│                       │" +
+			"│                       │" +
+			"│                       │" +
+			"│                       │" +
+			"│                       │" +
+			"│                       │" +
+			"│                       │" +
+			"└───────────────────────┘"
+
 	bg2 :=
-		"                         " + 
-		"                         " + 
-		"  /─\\                    " + 
-		"  \\─/                    " + 
-		"                         " + 
-		"          1234           " + 
-		"                         " + 
-		"                         " + 
-		"                         "
-		
+		"                         " +
+			"                         " +
+			"  /─\\                    " +
+			"  \\─/                    " +
+			"                         " +
+			"          1234           " +
+			"                         " +
+			"                         " +
+			"                         "
+
 	fg1 :=
-		"                         " + 
-		"                         " + 
-		"  ab                     " + 
-		"                         " + 
-		"                         " + 
-		"                         " + 
-		"                         " + 
-		"                         " + 
-		"                         "
-		
+		"                         " +
+			"                         " +
+			"  ab                     " +
+			"                         " +
+			"                         " +
+			"                         " +
+			"                         " +
+			"                         " +
+			"                         "
+
 	fg2 :=
-		"                         " + 
-		"                         " + 
-		"  c                      " + 
-		"  d                      " + 
-		"                         " + 
-		"                         " + 
-		"                         " + 
-		"                         " + 
-		"                         "
-	
+		"                         " +
+			"                         " +
+			"  c                      " +
+			"  d                      " +
+			"                         " +
+			"                         " +
+			"                         " +
+			"                         " +
+			"                         "
+
 	con.AddBackgroundS(bg1)
 	con.AddBackgroundS(bg2)
 	con.AddForegroundS(fg1)
 	con.AddForegroundS(fg2)
-	
+
 	con.CameraX = 0
 	con.CameraY = 0
-	
+
 	return con
 }
